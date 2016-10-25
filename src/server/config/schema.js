3a8cc3c5dd0114e430419f5735bf8ceac488433f
Himank Barve <hbarve1592@gmail.com>
2016-10-25 10:13:14 +0530

    Schema for Data in database is created.

---
+++ src/server/config/schema.js
@@ -0,0 +1,46 @@
+/**
+ * Created by himank on 1/8/16.
+ *
+ * This file covers all the input validation with Joi.js
+ * and it will export all variable further.
+ */
+
+import Joi from "joi";
+
+export const id       = Joi.string().length(24);
+export const mayashId = Joi.string().token().min(2).max(20);
+export const username = Joi.string().token().min(2).max(20);
+export const postId   = Joi.string().length(24);
+
+export const password = Joi.string().regex(/[a-zA-Z0-9]+/).min(5).max(30);
+export const mobile   = Joi.number().min(7000000000).max(9999999999).integer();
+export const email    = Joi.string().email();
+export const name     = Joi.string().min(3).max(30);
+
+export const circleType = Joi.string().valid('edu', 'org', 'location', 'field', 'social');
+export const status     = Joi.string().min(1).max(148);
+export const description= Joi.string().min(1).max(300);
+export const classroom  = Joi.boolean();
+
+
+export const courseId = Joi.string().length(24);
+export const moduleId = Joi.string();
+export const courseName  = Joi.string().min(3).max(148);
+export const courseUrl   = Joi.string().regex(/[a-zA-Z0-9-]+/);
+export const standard    = Joi.string().valid(
+                              'primary', 'secondary',
+                              'higher-secondary', 'graduation',
+                              'post-graduation', 'phd');
+export const level       = Joi.number().integer().min(1).max(5);
+
+
+export const moduleName  = Joi.string().min(3).max(148);
+export const moduleData  = Joi.string();
+
+
+export const articleId   = Joi.string().length(24);
+export const articleName = Joi.string().min(3).max(148);
+export const articleUrl  = Joi.string().regex(/[a-zA-Z0-9-]+/);
+export const articleData = Joi.string();
+
+//Cover rest all the things here.
