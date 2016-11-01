1b74f591ed3aa71b1f98caeed10144d3a03c337a
Himank Barve <hbarve1592@gmail.com>
2016-11-01 07:37:14 +0530

    font-awesome is shifted to cdn, and google analytic script is added in template()

--- src/server/plugins/views/index.js
+++ src/server/plugins/views/index.js
@@ -10,7 +10,7 @@ let template = () => {
       <!-- Tell the browser to be responsive to screen width -->
       <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   
-      <link rel="stylesheet" href="/node_modules/font-awesome/css/font-awesome.min.css">
+      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
       <!--<link rel="stylesheet" href="/style.css">-->
     </head>
     <body class="skin-purple layout-top-nav">
@@ -20,6 +20,19 @@ let template = () => {
         </div>
       </div>
       <script src="/bundle.js" ></script>
+      <script>
+        (function(i,s,o,g,r,a,m){
+          i['GoogleAnalyticsObject']=r;
+          i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];
+          a.async=1;
+          a.src=g;
+          m.parentNode.insertBefore(a,m);
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  
+        ga('create', 'UA-78687441-1', 'auto');
+        ga('send', 'pageview');
+      </script>
     </body>
     </html>`);
 };
