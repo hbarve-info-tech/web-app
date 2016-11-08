156a1259624d5558ed8b01474e6dbac933c7c10e
Himank Barve <hbarve1592@gmail.com>
2016-11-08 08:37:48 +0530

    color props is also added in error component

--- src/client/components/ErrorPage.js
+++ src/client/components/ErrorPage.js
@@ -4,17 +4,18 @@ import React from "react";
 const ErrorPage = ({
   statusCode     =404,
   messageHeading ='Oops! Page not found.',
-  message        = 'We could not find the page you were looking for. Meanwhile, you may return to dashboard.'
+  message        ='We could not find the page you were looking for. Meanwhile, you may return to dashboard.',
+  color          ='text-yellow'
 }) => {
   return (
     <section class="content">
       <div class="error-page">
-        <div class="headline text-yellow">
+        <div class={`headline ${color}`}>
           {statusCode}
         </div>
         <div>
           <h3>
-            <i class="fa fa-warning text-yellow"/>
+            <i class={`fa fa-warning ${color}`}/>
             {' ' + messageHeading}
           </h3>
           <p>{message}</p>
