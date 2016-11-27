2e76e37cb5d47dedf2809bc8b4d9add3a7faabb2
Himank Barve <hbarve1592@gmail.com>
2016-11-27 18:45:05 +0530

    template function is modified in view plugin

--- src/server/plugins/views/index.js
+++ src/server/plugins/views/index.js
@@ -1,40 +1,33 @@
 'use strict';
-let template = () => {
-  return (`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <title>Mayash</title>
-      <!-- Tell the browser to be responsive to screen width -->
-      <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  
-      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-      <!--<link rel="stylesheet" href="/style.css">-->
-    </head>
-    <body class="skin-purple layout-top-nav">
-      <div id="app">
-        <div id="loading">
-          Loading...
-        </div>
-      </div>
-      <script src="/bundle.js" ></script>
-      <script>
-        (function(i,s,o,g,r,a,m){
-          i['GoogleAnalyticsObject']=r;
-          i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];
-          a.async=1;
-          a.src=g;
-          m.parentNode.insertBefore(a,m);
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  
-        ga('create', 'UA-78687441-1', 'auto');
-        ga('send', 'pageview');
-      </script>
-    </body>
-    </html>`);
+let template = ({
+  title       = 'Mayash',
+  description = 'Mayash is a group of IITians committed for providing world class education at your door-step.',
+  keywords    = ['Mayash', 'Mayash education', 'IITians committed for providing world class education.']
+} = {}) => {
+  return (`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>${title}</title>
+  <meta name="description" content="${description}">
+  <meta name="keywords"    content="${keywords.toString()}">
+
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="/style.css">
+</head>
+<body class="skin-purple layout-top-nav">
+  <div id="app">
+    <div id="loading">
+      Loading...
+    </div>
+  </div>
+  <script src="/bundle.js" ></script>
+</body>
+</html>`);
 };
 
 export const register = (server, options, next) => {
@@ -47,7 +40,7 @@ export const register = (server, options, next) => {
 
     {method: 'GET', path:'/bundle.js',           handler: (request, reply) => reply.file('./public/bundle.js')},
     {method: 'GET', path:'/style.css',           handler: (request, reply) => reply.file('./public/style.css')},
-    {method: 'GET', path:'/favicon.ico',         handler: (request, reply) => reply.file('./public/favicon.ico')}
+    {method: 'GET', path:'/favicon.ico',         handler: (request, reply) => reply.file('./favicon.ico')}
   ]);
 
   next();
