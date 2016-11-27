b603f567ffee6bb4489f4c54918aaa63bc453f66
Himank Barve <hbarve1592@gmail.com>
2016-11-27 18:49:35 +0530

    circleMembers and circleCourses constants are added

--- src/server/database/setup.js
+++ src/server/database/setup.js
@@ -3,6 +3,8 @@ export const ELEMENTS = 'elements';
 export const ARTICLES = 'articles';
 export const COURSES  = 'courses';
 export const MODULES  = 'modules';
+export const CIRCLE_MEMBERS = 'circleMembers';
+export const CIRCLE_COURSES = 'circleCourses';
 
 import { datastore } from "google-cloud";
 import { gcloud }    from "../config";
