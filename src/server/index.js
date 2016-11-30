93a5c83a0c1e8fc5e1c7a11395d4ff29007e955d
Himank Barve <hbarve1592@gmail.com>
2016-11-30 12:31:03 +0530

    package.json scripts are modified, Root.dev.js file is modifed for client development and client redux development, and webpack dev server is added to main server file to run in development env

--- src/server/index.js
+++ src/server/index.js
@@ -6,6 +6,10 @@ import plugins    from "./plugins";
 const server = new Server();
 server.connection(config.server);
 
+if(process.env.NODE_ENV === 'development' && (process.env.DEV_ENV === 'client' || process.env.REDUX === true)) {
+  require('../../webpack');
+}
+
 server.register(plugins, (error) => {
   if(error) {
     throw error;
