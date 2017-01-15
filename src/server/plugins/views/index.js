131ac831fb50722f3494a6d4545305ccfb1021b5
Himank Barve <hbarve1592@gmail.com>
2017-01-15 07:36:27 +0530

    redux store is added to server side rendering

--- src/server/plugins/views/index.js
+++ src/server/plugins/views/index.js
@@ -4,10 +4,12 @@ const { NODE_ENV } = process.env;
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { match, RouterContext } from 'react-router';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-import App from "../../../client/App";
-import routes from '../../../client/routes';
-import NotFoundPage from '../../../client/components/NotFoundPage';
+import routes from '../../../client/routes/index';
+import NotFoundPage from '../../../client/routes/ErrorPage';
+import configureStore from '../../../client/store/configureStore';
 
 
 const googleAds = `<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
@@ -66,7 +68,7 @@ export const register = (server, options, next) => {
       method : 'GET',
       path   :'/{url*}',
       handler: (request, reply) => {
-        match( { routes, location: request.path}, (err, redirectLocation, renderProps) => {
+        match( { routes, location: request.path }, (err, redirectLocation, renderProps) => {
 
           // in case of error display the error message
           if (err) {
@@ -82,20 +84,26 @@ export const register = (server, options, next) => {
 
           // generate the React markup for the current route
           let markup;
+          let store = configureStore();
+
+          // store = configureStore();
+
           if (renderProps) {
             // if the current route matched we have renderProps
-            markup = renderToString(<RouterContext {...renderProps}/>);
+            markup = renderToString(
+              <Provider store={store}>
+                <RouterContext {...renderProps}/>
+              </Provider>
+            );
           } else {
             // otherwise we can render a 404 page
             markup = renderToString(<NotFoundPage/>);
-            // res.status(404);
           }
-
-          // render the index template with the embedded React markup
 
           return reply.view('index', {
             title  : 'Mayash',
-            app    : markup
+            app    : markup,
+            store  : JSON.stringify(store)
           });
         });
       }
