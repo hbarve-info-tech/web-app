f7ae15d455ac623ef908e9ca992ee44e32ed6052
Himank Barve <hbarve1592@gmail.com>
2017-01-20 10:30:51 +0530

    Server index.js file is updated

--- src/server/index.js
+++ src/server/index.js
@@ -1,33 +1,35 @@
-'use strict';
-import { Server } from "hapi";
-import Hoek       from "hoek";
-import config     from "./config";
-import plugins    from "./plugins";
+
+import { Server } from 'hapi';
+import Hoek from 'hoek';
+import Handlebars from 'handlebars';
+
+import { server as serverConfig } from './config';
+import plugins from './plugins';
 
 const server = new Server();
-server.connection(config.server);
+server.connection(serverConfig);
 
-if(process.env.NODE_ENV === 'development' && (process.env.DEV_ENV === 'client' || process.env.REDUX === true)) {
+if (process.env.NODE_ENV === 'development' && (process.env.DEV_ENV === 'client' || process.env.REDUX === true)) {
   require('../../webpack/devServer');
 }
 
-server.register(plugins, (error) => {
-  if(error) {
-    throw error;
+server.register(plugins, (error1) => {
+  if (error1) {
+    throw error1;
   }
   console.log('All plugins are added.');
 
   server.views({
-    engines   : { html: require('handlebars') },
-    relativeTo: __dirname + "/../../",
-    path      : 'public'
+    engines: { html: Handlebars },
+    relativeTo: `${__dirname}/../../`,
+    path: 'public',
   });
 
-  //Finally server is starting here.
-  server.start((error) => {
-    Hoek.assert(!error, error);
+  // Finally server is starting here.
+  server.start((error2) => {
+    Hoek.assert(!error2, error2);
 
-    console.log('Hapi server started @ ' + server.info.uri);
+    console.log(`Hapi server started @ ${server.info.uri}`);
   });
 });
 
