921715ada608c47db51b1c14d9132551e25d34c4
Himank Barve <hbarve1592@gmail.com>
2017-01-20 10:24:33 +0530

    database functions are updated

--- src/server/database/db.articles.js
+++ src/server/database/db.articles.js
@@ -1,199 +1,168 @@
-'use strict';
-import db, { ELEMENTS, ARTICLES} from "./setup";
+
+import db, { ELEMENTS, ARTICLES } from './setup';
 
 export const getArticleByArticleId = (articleId, callback) => {
-  let key = db.key([ARTICLES, parseInt(articleId, 10)]);
-  db.get(key, function(error, success) {
-    if(error) {
+  const key = db.key([ARTICLES, parseInt(articleId, 10)]);
+  db.get(key, (error, success) => {
+    if (error) {
       console.log(error);
 
       return callback({
         statusCode: 500,
-        error     : 'Database server error.',
-        message   : 'Database server error.'
+        error: 'Database server error.',
+        message: 'Database server error.',
       });
     }
 
-    if(success) {
+    if (success) {
       return callback({
         statusCode: 200,
-        message   : 'Success',
-        payload   : Object.assign(
+        message: 'Success',
+        payload: Object.assign(
           {},
           success,
           {
-            authorId : success.authorId.id,
-            articleId: success.articleId.id
-          }
-        )
+            authorId: success.authorId.id,
+            articleId: success.articleId.id,
+          },
+        ),
       });
     }
 
     return callback({
       statusCode: 404,
-      message   : 'Article does not exists.'
+      message: 'Article does not exists.',
     });
   });
 };
 
-//Return all the articles created by user with 'id'
+// Return all the articles created by user with 'id'
 export const getArticlesById = (id, callback) => {
-  var query = db.createQuery(ARTICLES)
+  const query = db.createQuery(ARTICLES)
                 .filter('authorId', '=', db.key([ELEMENTS, id]));
 
   db.runQuery(query, (err, articles) => {
-    if(err) {
+    if (err) {
       console.log(err);
 
       return callback({
         statusCode: 500,
-        error     : 'Database server error.',
-        message   : 'Database server error.'
+        error: 'Database server error.',
+        message: 'Database server error.',
       });
     }
 
-    if(articles.length === 0) {
+    if (articles.length === 0) {
       return callback({
         statusCode: 404,
-        error     : 'User does not exists.',
-        message   : 'User does not exists.'
+        error: 'User does not exists.',
+        message: 'User does not exists.',
       });
     }
-
-    articles = articles.map((article) => Object.assign(
-      {},
-      article,
-      {
-        articleId: article.articleId.id,
-        authorId : article.authorId.id
-      }
-    ));
 
     return callback({
       statusCode: 200,
-      message   : 'Successful.',
-      payload   : articles
+      message: 'Successful.',
+      payload: articles.map(a => ({ ...a, articleId: a.articleId.id, authorId: a.authorId.id })),
     });
   });
 };
 
-export const createArticleById    = (id, payload, callback) => {
-  let key = db.key(ARTICLES);
-
-  db.allocateIds(key, 1, (err, result) => {
-    if(err) {
+export const createArticleById = (id, payload, callback) => {
+  db.allocateIds(db.key(ARTICLES), 1, (err, keys) => {
+    if (err) {
       console.log(err);
 
       return callback({
         statusCode: 500,
-        error     : 'Server Error'
+        error: 'Server Error',
       });
     }
 
-    payload.authorId  = db.key([ELEMENTS, id]);
-    payload.articleId = db.key([ARTICLES, result[0].id]);
+    const key = db.key([ARTICLES, keys[0].id]);
+    const authorId = id;
+    const articleId = key.id;
 
-    db.save({
-      key : payload.articleId,
-      data: payload
-    }, (error, success) => {
+    const data = {
+      ...payload,
+      authorId: db.key([ELEMENTS, id]),
+      articleId: key,
+    };
+
+    db.save({ key, data }, (error) => {
       if (error) {
-        console.log (error);
+        console.log(error);
 
-        return callback ({
-          statusCode : 500,
-          error : 'Database server error.',
-          message : 'Database server error.'
+        return callback({
+          statusCode: 500,
+          error: 'Database server error.',
+          message: 'Database server error.',
         });
       }
 
-      return callback ({
-        statusCode : 201,
-        message    : 'Success',
-        payload    : {
-          authorId    : payload.authorId.id,
-          articleId   : payload.articleId.id,
-          articleName : payload.articleName
-        }
+      return callback({
+        statusCode: 201,
+        message: 'Success',
+        payload: { authorId, articleId },
       });
     });
   });
 };
 
 export const updateArticleByArticleId = (articleId, payload, callback) => {
-  let transaction = db.transaction();
+  const key = db.key([ARTICLES, articleId]);
+  db.get(key, (error1, article) => {
+    if (error1) {
+      console.log(error1);
 
-  transaction.run((err) => {
-    if(err) {
-      console.log(err);
       return callback({
-        statusCode : 500,
-        error      : 'Server error'
+        statusCode: 500,
+        error: 'Server error',
       });
     }
-    let key = db.key([ARTICLES, articleId]);
 
-    transaction.get(key, (err, data) => {
-      if(err) {
-        console.log(err);
+    const data = { ...article, ...payload };
+
+    db.save({ key, data }, (error2) => {
+      if (error2) {
+        console.log(error2);
+
         return callback({
-          statusCode : 500,
-          error      : 'Server error'
+          statusCode: 500,
+          error: 'Server error',
         });
       }
 
-      data = Object.assign(
-        {},
-        data,
-        payload
-      );
-
-      transaction.save({
-        key : key,
-        data : data
+      return callback({
+        statusCode: 200,
+        message: 'Update successfully.',
       });
-
-      transaction.commit((err) => {
-        if(err) {
-          console.log(err);
-          return callback({
-            statusCode : 500,
-            error      : 'Server error'
-          });
-        }
-
-        return callback({
-          statusCode : 200,
-          message    : 'Update successfully.'
-        });
-      })
     });
-
   });
 };
 
-//For now it is working but it is incomplete.
+// For now it is working but it is incomplete.
 export const deleteArticleByArticleId = (articleId, callback) => {
   db.delete(db.key([ARTICLES, articleId]), (err, result) => {
-    if(err) {
+    if (err) {
       console.log(err);
 
       return callback({
         statusCode: 500,
-        error     : 'server error'
+        error: 'server error',
       });
     }
 
-    if(result.indexUpdates === 0) {
+    if (result.indexUpdates === 0) {
       return callback({
         statusCode: 404,
-        error     : 'Article Not found.'
+        error: 'Article Not found.',
       });
     }
 
     return callback({
       statusCode: 200,
-      message   : 'Successfully deleted'
+      message: 'Successfully deleted',
     });
   });
 };
