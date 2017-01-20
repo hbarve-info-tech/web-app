0a2a035cb424fcd3a084e6f53f61af5253ea4f8a
Himank Barve <hbarve1592@gmail.com>
2017-01-20 11:19:01 +0530

    Client reducers are updated

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,7 +1,7 @@
-'use strict';
+
 import { combineReducers } from 'redux';
-import user     from './user';
+import user from './user';
 
 export default combineReducers({
-  user
+  user,
 });
--- src/client/reducers/user.js
+++ src/client/reducers/user.js
@@ -1,59 +1,58 @@
-"use strict";
-import { USER_SIGN_IN, USER_SIGN_IN_START, USER_SIGN_IN_ERROR, USER_SIGN_IN_SUCCESS } from '../actions/user';
 
-import { readLocalStore, writeLocalStore, removeLocalStore,
-  readCookie, writeCookie, removeAllCookies } from "../api/clientApi";
+import { USER_SIGN_IN_START, USER_SIGN_IN_ERROR, USER_SIGN_IN_SUCCESS } from '../actions/user';
+
+import { writeLocalStore, writeCookie } from '../api/clientApi';
 
 const initialState = {
-  isSigningIn : false,
-  isSignedIn  : false,
+  isSigningIn: false,
+  isSignedIn: false,
 
-  isFetching  : false,
-  isFetched   : false,
+  isFetching: false,
+  isFetched: false,
 
-  isError     : false,
-  error       : '',
-  message     : '',
+  isError: false,
+  error: '',
+  message: '',
 
-  lastUpdated : Date.now()
+  lastUpdated: Date.now(),
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case USER_SIGN_IN_START  : {
+    case USER_SIGN_IN_START : {
       return {
         ...state,
         isSigningIn: true,
-        isSignedIn : false,
+        isSignedIn: false,
 
-        isFetching : false,
-        isFetched  : false,
+        isFetching: false,
+        isFetched: false,
 
-        isError    : false,
-        error      : '',
-        message    : '',
+        isError: false,
+        error: '',
+        message: '',
 
-        lastUpdated: Date.now()
+        lastUpdated: Date.now(),
       };
     }
-    case USER_SIGN_IN_ERROR  : {
+    case USER_SIGN_IN_ERROR : {
       return {
         ...state,
         isSigningIn: false,
-        isSignedIn : false,
+        isSignedIn: false,
 
-        isFetching : false,
-        isFetched  : false,
+        isFetching: false,
+        isFetched: false,
 
-        isError    : true,
-        error      : action.payload.error,
-        message    : action.payload.message,
+        isError: true,
+        error: action.payload.error,
+        message: action.payload.message,
 
-        lastUpdated: Date.now()
+        lastUpdated: Date.now(),
       };
     }
     case USER_SIGN_IN_SUCCESS: {
-      let { payload } = action;
+      const { payload } = action;
 
       writeLocalStore('user', payload);
       writeCookie('id', payload.id);
@@ -64,16 +63,16 @@ export default (state = initialState, action) => {
         ...payload,
         ...state,
         isSigningIn: false,
-        isSignedIn : true,
+        isSignedIn: true,
 
-        isFetching : false,
-        isFetched  : false,
+        isFetching: false,
+        isFetched: false,
 
-        isError    : false,
-        error      : '',
-        message    : '',
+        isError: false,
+        error: '',
+        message: '',
 
-        lastUpdated: Date.now()
+        lastUpdated: Date.now(),
       };
     }
 
