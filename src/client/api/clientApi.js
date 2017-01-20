0295a754514a98db6831eeb72da9c1c9083a9f79
Himank Barve <hbarve1592@gmail.com>
2017-01-20 10:54:36 +0530

    API files are updated

--- src/client/api/clientApi.js
+++ src/client/api/clientApi.js
@@ -1,78 +1,62 @@
-'use strict';
 
 export const readLocalStore = (key) => {
   // Code for localStorage/sessionStorage.
-  if (typeof(Storage) !== "undefined") {
+  if (typeof (Storage) !== 'undefined') {
     return JSON.parse(window.localStorage.getItem(key));
   }
-  // Sorry! No Web Storage support..
-  else {
-    return null;
-  }
-
+  return null;
 };
-
 
 export const writeLocalStore = (key, value) => {
-  if (typeof(Storage) !== "undefined") {
+  if (typeof (Storage) !== 'undefined') {
     window.localStorage.setItem(key, JSON.stringify(value));
     return true;
-  } else {
-    // Sorry! No Web Storage support..
-    return false;
   }
+  return false;
 };
 
-
 export const removeLocalStore = (key) => {
-  if (typeof(Storage) !== "undefined") {
+  if (typeof (Storage) !== 'undefined') {
     window.localStorage.removeItem(key);
     return true;
   }
-  else {
-    // Sorry! No Web Storage support..
-    return false;
-  }
+  return false;
 };
 
-
 export const readCookie = (key) => {
-  let name = key + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  for(let i = 0; i <ca.length; i++) {
+  const name = `${key}=`;
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(';');
+  for (let i = 0; i < ca.length; i += 1) {
     let c = ca[i];
-    while (c.charAt(0) == ' ') {
+    while (c.charAt(0) === ' ') {
       c = c.substring(1);
     }
-    if (c.indexOf(name) == 0) {
+    if (c.indexOf(name) === 0) {
       return c.substring(name.length, c.length);
     }
   }
-  return "";
+  return '';
 };
 
-
-export const writeCookie = (key, value, expdays= 10) => {
-  let d = new Date();
-  d.setTime(d.getTime() + (expdays*24*60*60*1000));
-  let expires = "expires="+ d.toUTCString();
+export const writeCookie = (key, value, expdays = 10) => {
+  const d = new Date();
+  d.setTime(d.getTime() + (expdays * 24 * 60 * 60 * 1000));
+  const expires = `expires=${d.toUTCString()}`;
   document.cookie = `${key}=${value};${expires};path=/`;
 };
-
 
 export const removeCookie = (key) => {
   document.cookie = `${key}=;expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
 };
 
-
 export const removeAllCookies = () => {
-  let cookies = document.cookie.split(";");
+  const cookies = document.cookie.split(';');
 
-  for (let i = 0; i < cookies.length; i++) {
-    let cookie = cookies[i];
-    let eqPos = cookie.indexOf("=");
-    let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+  for (let i = 0; i < cookies.length; i += 1) {
+    const cookie = cookies[i];
+    const eqPos = cookie.indexOf('=');
+    const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT`;
   }
 };
