41e431c6887bb516de2fdbf4338b2c97f9b8f390
Himank Barve <hbarve1592@gmail.com>
2017-01-21 21:04:08 +0530

    Home component is improved

--- src/client/components/Home/index.js
+++ src/client/components/Home/index.js
@@ -1,4 +1,70 @@
 
 import React from 'react';
+import Component from 'react/lib/ReactComponent';
+import PropTypes from 'react/lib/ReactPropTypes';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
-export default () => (<div>This is Home Component.</div>);
+import actions from '../../actions';
+
+import ProfileInfo from '../ProfileInfo';
+import PostCreate from '../PostCreate';
+import Timeline from '../Timeline';
+
+const IsClient = typeof document === 'object';
+
+if (IsClient) {
+  require('./Home.scss');
+}
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="mdl-grid">
+        <div className="mdl-cell mdl-cell--2-col mdl-cell--2-col-tablet mdl-cell--4-col-phone">
+          <ProfileInfo
+            name="Himank Barve"
+            username="hbarve1"
+            profilePic="https://storage.googleapis.com/mayash/12993536_1078754218834834_8314114430867694644_n.jpg"
+            classroom={false}
+          />
+        </div>
+        <div className="mdl-cell mdl-cell--8-col mdl-cell--5-col-tablet mdl-cell--4-col-phone">
+          <Timeline
+            posts={[]}
+            timelineType="article"
+          />
+        </div>
+        <PostCreate />
+      </div>
+    );
+  }
+}
+
+Home.propTypes = {
+  user: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    username: PropTypes.string,
+    token: PropTypes.string,
+    profilePic: PropTypes.string,
+    isSigningIn: PropTypes.bool,
+    isSignedIn: PropTypes.bool,
+    isFetching: PropTypes.bool,
+    isFetched: PropTypes.bool,
+    isError: PropTypes.bool,
+    error: PropTypes.string,
+    message: PropTypes.string,
+    lastUpdated: PropTypes.number,
+  }).isRequired,
+};
+
+const mapStateToProps = state => state;
+const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
