09d65379565ad6e50b8073e3600eb43bba1ec3f7
Himank Barve <hbarve1592@gmail.com>
2017-01-21 21:01:52 +0530

    Timeline component is improved

--- src/client/components/Timeline/index.js
+++ src/client/components/Timeline/index.js
@@ -1,4 +1,34 @@
 
 import React from 'react';
+import PropTypes from 'react/lib/ReactPropTypes';
 
-export default () => (<div>This is Timeline Component.</div>);
+import Post from '../Post';
+
+const IsClient = typeof document === 'object';
+
+if (IsClient) {
+  require('./Timeline.scss');
+}
+
+const Timeline = ({ posts, timelineType }) => {
+  const postType = timelineType;
+
+  return (
+    <div className="timeline">
+      {posts.length ? posts.map(post => (
+        <Post
+          post={post}
+          postType={postType}
+          key={postType === 'article' ? post.articleId : post.courseId}
+        />
+      )) : <Post /> }
+    </div>
+  );
+};
+
+Timeline.propTypes = {
+  posts: PropTypes.array.isRequired,
+  timelineType: PropTypes.string.isRequired,
+};
+
+export default Timeline;
