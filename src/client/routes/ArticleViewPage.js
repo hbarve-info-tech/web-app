f06a39fc6a486f0e125e415189841ca45892064c
Himank Barve <hbarve1592@gmail.com>
2017-01-22 18:26:33 +0530

    ArticleViewPage component is created

--- src/client/routes/ArticleViewPage.js
+++ src/client/routes/ArticleViewPage.js
@@ -1,4 +1,81 @@
 
 import React from 'react';
+import Component from 'react/lib/ReactComponent';
+import PropTypes from 'react/lib/ReactPropTypes';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
-export default () => (<div>This is Article View Component.</div>);
+import actions from '../actions';
+import Article from '../components/Article';
+
+class ArticleViewPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentWillMount() {
+    const { articleId } = this.props.routeParams;
+    const article = this.props.articles.array.find(a => a.articleId === parseInt(articleId, 10));
+    if (!article) {
+      const { token } = this.props.user;
+      this.props.fetchArticle({ articleId, token });
+    }
+  }
+
+  render() {
+    const { articleId } = this.props.routeParams;
+    const article = this.props.articles.array.find(a => a.articleId === parseInt(articleId, 10));
+
+    return (
+      <div className="mdl-grid mdl-shadow--4dp mayash-article-view-page">
+        <div className="mdl-cell mdl-cell--8-col mdl-cell--6-col-tablet mdl-cell--4-col-phone">
+          <Article {...article} />
+        </div>
+        <div className="mdl-cell mdl-cell--4-col mdl-cell--2-col-tablet mdl-cell--4-col-phone" />
+      </div>
+    );
+  }
+}
+
+ArticleViewPage.propTypes = {
+  user: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    username: PropTypes.string,
+    token: PropTypes.string,
+    profilePic: PropTypes.string,
+    isSigningIn: PropTypes.bool,
+    isSignedIn: PropTypes.bool,
+    isFetching: PropTypes.bool,
+    isFetched: PropTypes.bool,
+    isError: PropTypes.bool,
+    error: PropTypes.string,
+    message: PropTypes.string,
+    lastUpdated: PropTypes.number,
+  }).isRequired,
+  articles: PropTypes.shape({
+    array: PropTypes.arrayOf(PropTypes.object),
+    isCreating: PropTypes.bool,
+    isUpdating: PropTypes.bool,
+    isFetching: PropTypes.bool,
+    isDeleting: PropTypes.bool,
+    isCreated: PropTypes.bool,
+    isUpdated: PropTypes.bool,
+    isFetched: PropTypes.bool,
+    isDeleted: PropTypes.bool,
+    isError: PropTypes.bool,
+    error: PropTypes.string,
+    message: PropTypes.string,
+    lastUpdated: PropTypes.number,
+  }).isRequired,
+  routeParams: PropTypes.shape({
+    articleId: PropTypes.string.isRequired,
+  }).isRequired,
+  fetchArticle: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => state;
+const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(ArticleViewPage);
