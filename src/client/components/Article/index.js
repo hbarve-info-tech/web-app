f27fe65555d7893dcf6c58045aae8ee245b4ab0b
Himank Barve <hbarve1592@gmail.com>
2017-01-22 17:39:20 +0530

    Article component is improved

--- src/client/components/Article/index.js
+++ src/client/components/Article/index.js
@@ -1,4 +1,45 @@
 
 import React from 'react';
+import PropTypes from 'react/lib/ReactPropTypes';
+import { Editor, createEditorState } from 'medium-draft';
 
-export default () => (<div>This is Article Component.</div>);
+const IsClient = typeof document === 'object';
+
+if (IsClient) {
+  require('medium-draft/lib/index.css');
+  require('./Article.scss');
+}
+
+const Article = ({ articleId, articleName, description, articleData }) => {
+  const editorState = createEditorState(articleData);
+  const onChange = () => {};
+
+  return (
+    <div className="mdl-card mayash-article">
+      <div className="mdl-card__title">
+        <h2 className="mdl-card__title-text">
+          {articleName}
+        </h2>
+      </div>
+      <div className="mdl-card__supporting-text">
+        <Editor
+          editorState={editorState}
+          onChange={onChange}
+          editorEnabled={false}
+        />
+      </div>
+    </div>
+  );
+};
+
+Article.propTypes = {
+  articleId: PropTypes.number.isRequired,
+  articleName: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  articleData: PropTypes.shape({
+    entityMap: PropTypes.shape({}),
+    blocks: PropTypes.arrayOf(PropTypes.object),
+  }).isRequired,
+};
+
+export default Article;
