ad0ec9da3aa1fcd8004085c63e026c5d849cfb1c
Himank Barve <hbarve1592@gmail.com>
2017-01-23 10:28:33 +0530

    getElement api function is added

--- src/client/api/index.js
+++ src/client/api/index.js
@@ -35,6 +35,21 @@ export const fetchUser = ({ id, token }, callback) => {
     .then(json => callback(json));
 };
 
+export const getElement = ({ id, username, token }, callback) => {
+  const url = `${HOST}/api/elements?${typeof id === 'undefined' ? `username=${username}` : `id=${id}`}`;
+
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+  })
+    .then(response => response.json())
+    .then(json => callback(json));
+};
+
 export const getArticle = ({ articleId, token }, callback) => {
   const url = `${HOST}/api/articles/${articleId}`;
   const headers = {
