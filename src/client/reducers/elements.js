5e17e5a1f38ccffcea22b28918dcb510e679b95e
Himank Barve <hbarve1592@gmail.com>
2017-01-23 10:32:33 +0530

    elements reducer is added

---
+++ src/client/reducers/elements.js
@@ -0,0 +1,135 @@
+
+import { ELEMENT_FETCH_START, ELEMENT_FETCH_SUCCESS, ELEMENT_FETCH_ERROR } from '../actions/elements';
+
+export const initialElementState = {
+  isFetching: false,
+  isFetched: false,
+
+  statusCode: 200,
+  isError: false,
+  error: '',
+
+  message: '',
+  lastUpdated: Date.now(),
+};
+const initialState = {
+  array: [],
+
+  isFetching: false,
+  isFetched: false,
+
+  statusCode: 200,
+  isError: false,
+  error: '',
+
+  message: '',
+  lastUpdated: Date.now(),
+};
+
+const elementReducer = (state = initialElementState, action) => {
+  switch (action.type) {
+    case ELEMENT_FETCH_START: {
+      return {
+        ...state,
+        ...action.payload,
+        isFetching: true,
+        lastUpdated: Date.now(),
+      };
+    }
+    case ELEMENT_FETCH_ERROR: {
+      return {
+        ...state,
+        isFetched: false,
+        isFetching: true,
+        isError: true,
+        error: action.error,
+        message: action.message,
+      };
+    }
+    case ELEMENT_FETCH_SUCCESS: {
+      return {
+        ...state,
+        ...action.payload,
+        isFetching: false,
+        isFetched: true,
+        lastUpdated: Date.now(),
+      };
+    }
+
+    default:
+      return state;
+  }
+};
+
+const elementsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ELEMENT_FETCH_START: {
+      const { username, id } = action.payload;
+      let index = -1;
+
+      if (username) {
+        index = state.array.findIndex(element => element.username === username);
+      }
+      else if (id) {
+        index = state.array.findIndex(element => element.id === parseInt(id, 10));
+      }
+
+      return {
+        ...state,
+        array: [
+          ...state.array.slice(0, index),
+          elementReducer(undefined, action),
+          ...state.array.slice(index + 1, state.array.length),
+        ],
+      };
+    }
+    case ELEMENT_FETCH_ERROR: {
+      const { username, id } = action.payload;
+      let index = -1;
+
+      if (username) {
+        index = state.array.findIndex(element => element.username === username);
+      }
+      else if (id) {
+        index = state.array.findIndex(element => element.id === parseInt(id, 10));
+      }
+
+      return {
+        ...state,
+        array: [
+          ...state.array.slice(0, index),
+          elementReducer(state.array[index], action),
+          ...state.array.slice(index + 1, state.array.length),
+        ],
+        lastUpdated: Date.now(),
+      };
+    }
+    case ELEMENT_FETCH_SUCCESS: {
+      const { array } = state;
+      const { username, id } = action.payload;
+      let index = -1;
+
+      if (username) {
+        index = state.array.findIndex(element => element.username === username);
+      }
+      else if (id) {
+        index = state.array.findIndex(element => element.id === parseInt(id, 10));
+      }
+
+      return {
+        ...state,
+        array: [
+          ...array.slice(0, index),
+          elementReducer(array[index], action),
+          ...array.slice(index + 1, array.length),
+        ],
+        lastUpdated: Date.now(),
+      };
+    }
+
+    default:
+      return state;
+  }
+};
+
+export default elementsReducer;
--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,9 +1,11 @@
 
 import { combineReducers } from 'redux';
 import user from './user';
+import elements from './elements';
 import articles from './articles';
 
 export default combineReducers({
   user,
+  elements,
   articles,
 });
