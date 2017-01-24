52a35a3390b35b71768c53ae4a201636b2548bf3
Himank Barve <hbarve1592@gmail.com>
2017-01-24 09:50:19 +0530

    course and classroom actions are added to index.js

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -2,9 +2,13 @@
 import * as user from './user';
 import * as elements from './elements';
 import * as articles from './articles';
+import * as courses from './courses';
+import * as classroom from './classroom';
 
 export default {
   ...user,
   ...elements,
   ...articles,
+  ...courses,
+  ...classroom,
 };
