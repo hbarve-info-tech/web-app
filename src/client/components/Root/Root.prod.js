c088c639467f8121fd2d2b46b976f7d3d5405942
Himank Barve <hbarve1592@gmail.com>
2017-01-24 19:54:03 +0530

    import routes bug is removed

--- src/client/components/Root/Root.prod.js
+++ src/client/components/Root/Root.prod.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
-import routes from '../components/Routes';
+import routes from '../Routes';
 
 export default ({ store }) => (
   <Provider store={store}>
