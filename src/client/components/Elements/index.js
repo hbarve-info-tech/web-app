b47d89419ab7c88dc7aeb8df8189b225a89dd4f7
Himank Barve <hbarve1592@gmail.com>
2017-01-24 10:06:01 +0530

    Element component is improved

---
+++ src/client/components/Elements/index.js
@@ -0,0 +1,80 @@
+
+import React from 'react';
+import Component from 'react/lib/ReactComponent';
+import PropTypes from 'react/lib/ReactPropTypes';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import actions from '../../actions';
+
+import ProfileInfo from '../ProfileInfo';
+import Timeline from '../Timeline';
+
+const IsClient = typeof document === 'object';
+
+if (IsClient) {
+  require('./Elements.scss');
+}
+
+class Elements extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    const { id, token } = this.props.user;
+    this.props.fetchArticles({ id, token });
+  }
+
+  render() {
+    const { user } = this.props;
+    const articles = this.props.articles.array;
+
+    return (
+      <div className="mdl-grid">
+        <div className="mdl-cell mdl-cell--2-col mdl-cell--2-col-tablet mdl-cell--4-col-phone">
+          <ProfileInfo
+            name={user.name}
+            username={user.username}
+            profilePic={user.profilePic}
+            classroom={user.classroom}
+          />
+        </div>
+        <div className="mdl-cell mdl-cell--8-col mdl-cell--5-col-tablet mdl-cell--4-col-phone">
+          <Timeline
+            posts={articles}
+            timelineType="article"
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+Elements.propTypes = {
+  user: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    username: PropTypes.string,
+    token: PropTypes.string,
+    profilePic: PropTypes.string,
+    isSigningIn: PropTypes.bool,
+    isSignedIn: PropTypes.bool,
+    isFetching: PropTypes.bool,
+    isFetched: PropTypes.bool,
+    isError: PropTypes.bool,
+    error: PropTypes.string,
+    message: PropTypes.string,
+    lastUpdated: PropTypes.number,
+  }).isRequired,
+  fetchArticles: PropTypes.func.isRequired,
+  articles: PropTypes.shape({
+    array: PropTypes.array.isRequired,
+  }).isRequired,
+};
+
+const mapStateToProps = state => state;
+const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Elements);
