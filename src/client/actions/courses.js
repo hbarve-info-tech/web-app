fdb593273ea678abb963e920f04138e3e76cda79
Himank Barve <hbarve1592@gmail.com>
2017-01-28 00:28:18 +0530

    actions for coruses are updated

--- src/client/actions/courses.js
+++ src/client/actions/courses.js
@@ -13,6 +13,12 @@ export const COURSE_FETCH_START = 'COURSE_FETCH_START';
 export const COURSE_FETCH_SUCCESS = 'COURSE_FETCH_SUCCESS';
 export const COURSE_FETCH_ERROR = 'COURSE_FETCH_ERROR';
 
+export const COURSE_UPDATE = 'COURSE_UPDATE';
+export const COURSE_UPDATE_START = 'COURSE_UPDATE_START';
+export const COURSE_UPDATE_SUCCESS = 'COURSE_UPDATE_SUCCESS';
+export const COURSE_UPDATE_ERROR = 'COURSE_UPDATE_ERROR';
+
+
 export const MODULES_FETCH = 'MODULES_FETCH';
 export const MODULES_FETCH_START = 'MODULES_FETCH_START';
 export const MODULES_FETCH_SUCCESS = 'MODULES_FETCH_SUCCESS';
@@ -23,19 +29,24 @@ export const MODULE_FETCH_START = 'MODULE_FETCH_START';
 export const MODULE_FETCH_SUCCESS = 'MODULE_FETCH_SUCCESS';
 export const MODULE_FETCH_ERROR = 'MODULE_FETCH_ERROR';
 
+export const MODULE_UPDATE = 'MODULE_UPDATE';
+export const MODULE_UPDATE_START = 'MODULE_UPDATE_START';
+export const MODULE_UPDATE_SUCCESS = 'MODULE_UPDATE_SUCCESS';
+export const MODULE_UPDATE_ERROR = 'MODULE_UPDATE_ERROR';
+
 
 const fetchCourseStart = payload => ({ type: COURSE_FETCH_START, payload });
 const fetchCourseSuccess = payload => ({ type: COURSE_FETCH_SUCCESS, payload });
 const fetchCourseError = payload => ({ type: COURSE_FETCH_ERROR, payload });
 export const fetchCourse = ({ courseId, token }) => (dispatch) => {
-  dispatch(fetchCourseStart({ courseId }));
+  dispatch(fetchCourseStart({ courseId: parseInt(courseId, 10) }));
 
   api.getCourse({ courseId, token }, (json) => {
     if (json.statusCode === 200) {
-      dispatch(fetchCourseSuccess(json.payload));
+      dispatch(fetchCourseSuccess({ ...json.payload, courseId: parseInt(courseId, 10) }));
     }
     else if (json.statusCode >= 400) {
-      dispatch(fetchCourseError(json));
+      dispatch(fetchCourseError({ courseId: parseInt(courseId, 10), ...json }));
     }
   });
 };
@@ -55,6 +66,55 @@ export const fetchCourses = ({ id, token }) => (dispatch) => {
     }
   });
 };
+
+const updateCourseStart = payload => ({ type: COURSE_UPDATE_START, payload });
+const updateCourseSuccess = payload => ({ type: COURSE_UPDATE_SUCCESS, payload });
+const updateCourseError = payload => ({ type: COURSE_UPDATE_ERROR, payload });
+export const updateCourse = ({
+  id,
+  token,
+  courseId,
+  courseName,
+  description,
+  standard,
+  level,
+}) => (dispatch) => {
+  dispatch(updateCourseStart({ courseId: parseInt(courseId, 10) }));
+
+  api.updateCourse({
+    id,
+    token,
+    courseId,
+    courseName,
+    description,
+    standard,
+    level,
+  }, (json) => {
+    console.log(json);
+    if (json.statusCode === 200) {
+      const payload = { courseId: parseInt(courseId, 10) };
+
+      if (courseName) {
+        payload.courseName = courseName;
+      }
+      if (description) {
+        payload.description = description;
+      }
+      if (standard) {
+        payload.standard = standard;
+      }
+      if (level) {
+        payload.level = level;
+      }
+
+      dispatch(updateCourseSuccess(payload));
+    }
+    else if (json.statusCode >= 400) {
+      dispatch(updateCourseError(json));
+    }
+  });
+};
+
 
 const fetchModuleStart = payload => ({ type: MODULE_FETCH_START, payload });
 const fetchModuleSuccess = payload => ({ type: MODULE_FETCH_SUCCESS, payload });
@@ -84,6 +144,47 @@ export const fetchModules = ({ courseId, token }) => (dispatch) => {
     }
     else if (json.statusCode >= 400) {
       dispatch(fetchModulesError(json));
+    }
+  });
+};
+
+const updateModuleStart = payload => ({ type: MODULE_UPDATE_START, payload });
+const updateModuleError = payload => ({ type: MODULE_UPDATE_ERROR, payload });
+const updateModuleSuccess = payload => ({ type: MODULE_UPDATE_SUCCESS, payload });
+export const updateModule = ({
+  id,
+  token,
+  courseId,
+  moduleId,
+  moduleName,
+  moduleData,
+}) => (dispatch) => {
+  const payload = {
+    courseId: parseInt(courseId, 10),
+    moduleId: parseInt(moduleId, 10),
+  };
+  dispatch(updateModuleStart(payload));
+
+  api.updateModule({
+    id,
+    token,
+    courseId,
+    moduleId,
+    moduleName,
+    moduleData,
+  }, (json) => {
+    if (json.statusCode === 200) {
+      if (moduleName) {
+        payload.moduleName = moduleName;
+      }
+      if (moduleData) {
+        payload.moduleData = moduleData;
+      }
+
+      dispatch(updateModuleSuccess(payload));
+    }
+    else if (json.statusCode >= 400) {
+      dispatch(updateModuleError({ ...payload, ...json }));
     }
   });
 };
