73a6c4700f9c0d520fc506e444bd6635cd57bd9f
Himank Barve <hbarve1592@gmail.com>
2017-01-30 21:43:43 +0530

    View plugin is divided in parts, all route config are seperated into different files

---
+++ src/server/plugins/views/Classroom.js
@@ -0,0 +1,96 @@
+
+import Boom from 'boom';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { match, RouterContext } from 'react-router';
+import { Provider } from 'react-redux';
+import Joi from 'joi';
+
+import { Username } from '../../config/schema';
+
+import actions from '../../../client/actions';
+
+import ReactRoutes from '../../../client/components/Routes';
+import NotFoundPage from '../../../client/components/Error';
+import configureStore from '../../../client/store/configureStore';
+
+const { NODE_ENV } = process.env;
+
+export default {
+  validate: {
+    params: Joi.object({
+      username: Username.required(),
+    }),
+  },
+  handler: (request, reply) => {
+    if (request.path !== request.path.toLowerCase()) {
+      return reply.redirect(request.path.toLowerCase());
+    }
+
+    match({
+      routes: ReactRoutes,
+      location: request.path,
+    }, (err, redirectLocation, renderProps) => {
+      if (err) {
+        console.log(err);
+        return reply(Boom.badImplementation());
+      }
+
+      if (redirectLocation) {
+        return reply.redirect(redirectLocation.pathname);
+      }
+
+      const context = {
+        title: 'Mayash',
+        description: 'Transforming Education.',
+        keywords: 'education, courses',
+        imageUrl: 'https://storage.googleapis.com/mayash/website/mayash-title.png',
+        type: 'website',
+        url: request.path,
+        site_name: 'Transforming Education',
+        app: '',
+        initialState: '',
+        PRODUCTION: NODE_ENV === 'production',
+      };
+
+      if (!renderProps) {
+        context.app = renderToString(<NotFoundPage />);
+        return reply.view('index', context);
+      }
+
+      let store = configureStore();
+      const initialState = store.getState();
+
+      const { isSignedIn, id, token } = request.state;
+      const { user } = initialState;
+
+      if (isSignedIn === 'true') {
+        store = configureStore({
+          ...initialState,
+          user: {
+            ...user,
+            isSignedIn: true,
+            id: parseInt(id, 10),
+            token,
+          },
+        });
+      }
+
+      const { username } = request.params;
+
+      store.dispatch(actions.fetchElement({ username, token }));
+
+      const unsubscribe = store.subscribe(() => {
+        context.app = renderToString(
+          <Provider store={store}>
+            <RouterContext {...renderProps} />
+          </Provider>,
+        );
+
+        context.initialState = JSON.stringify(store.getState());
+        unsubscribe();
+        return reply.view('index', context);
+      });
+    });
+  },
+};
---
+++ src/server/plugins/views/Others.js
@@ -0,0 +1,79 @@
+
+import Boom from 'boom';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { match, RouterContext } from 'react-router';
+import { Provider } from 'react-redux';
+
+import ReactRoutes from '../../../client/components/Routes';
+import NotFoundPage from '../../../client/components/Error';
+import configureStore from '../../../client/store/configureStore';
+
+const { NODE_ENV } = process.env;
+
+export default {
+  handler: (request, reply) => {
+    if (request.path !== request.path.toLowerCase()) {
+      return reply.redirect(request.path.toLowerCase());
+    }
+
+    match({
+      routes: ReactRoutes,
+      location: request.path,
+    }, (err, redirectLocation, renderProps) => {
+      if (err) {
+        console.log(err);
+        return reply(Boom.badImplementation());
+      }
+
+      if (redirectLocation) {
+        return reply.redirect(redirectLocation.pathname);
+      }
+
+      const context = {
+        title: 'Mayash',
+        description: 'Transforming Education.',
+        keywords: 'education, courses',
+        imageUrl: 'https://storage.googleapis.com/mayash/website/mayash-title.png',
+        type: 'website',
+        url: request.path,
+        site_name: 'Transforming Education',
+        app: '',
+        initialState: '',
+        PRODUCTION: NODE_ENV === 'production',
+      };
+
+      if (!renderProps) {
+        context.app = renderToString(<NotFoundPage />);
+        return reply.view('index', context);
+      }
+
+      let store = configureStore();
+      const initialState = store.getState();
+      context.initialState = JSON.stringify(initialState);
+
+      const { isSignedIn, id, token } = request.state;
+
+      if (isSignedIn === 'true') {
+        const { user } = initialState;
+        store = configureStore({
+          ...initialState,
+          user: {
+            ...user,
+            isSignedIn: true,
+            id: parseInt(id, 10),
+            token,
+          },
+        });
+      }
+      context.initialState = JSON.stringify(store.getState());
+
+      context.app = renderToString(
+        <Provider store={store}>
+          <RouterContext {...renderProps} />
+        </Provider>,
+      );
+      return reply.view('index', context);
+    });
+  },
+};
