d646e440cc1f5eaddb0c23788fd83dff352bf6d1
Himank Barve <hbarve1592@gmail.com>
2017-01-30 23:00:49 +0530

    Little more improvement is done in View plugins

--- src/server/plugins/views/AboutUs.js
+++ src/server/plugins/views/AboutUs.js
@@ -12,68 +12,73 @@ import configureStore from '../../../client/store/configureStore';
 const { NODE_ENV } = process.env;
 
 export default {
-  handler: (request, reply) => {
-    if (request.path !== request.path.toLowerCase()) {
-      return reply.redirect(request.path.toLowerCase());
+  handler: (request, reply) => match({
+    routes: ReactRoutes,
+    location: request.path,
+  }, (err, redirectLocation, renderProps) => {
+    if (err) {
+      console.log(err);
+      return reply(Boom.badImplementation());
     }
 
-    match({
-      routes: ReactRoutes,
-      location: request.path,
-    }, (err, redirectLocation, renderProps) => {
-      if (err) {
-        console.log(err);
-        return reply(Boom.badImplementation());
-      }
+    if (redirectLocation) {
+      return reply.redirect(redirectLocation.pathname);
+    }
 
-      if (redirectLocation) {
-        return reply.redirect(redirectLocation.pathname);
-      }
+    const context = {
+      title: 'About Us',
+      description: ['Mayash is a learning and sharing platform, where all the educational ',
+        'institute, teachers, professional, students can learn and share their knowledge. Here we ',
+        'are covering all the fields of education with freedom of language.'].join(),
+      keywords: [
+        'mayash',
+        'mayash.com',
+        'mayash.in',
+        'mayash.xyz',
+        'mayash.edu',
+        'mayash education',
+        'online classroom',
+        'online courses',
+      ].join(),
+      imageUrl: 'https://storage.googleapis.com/mayash/website/mayash-title.png',
+      type: 'website',
+      url: request.path,
+      site_name: 'Transforming Education',
+      app: '',
+      initialState: '',
+      PRODUCTION: NODE_ENV === 'production',
+    };
 
-      const context = {
-        title: 'Mayash',
-        description: 'Transforming Education.',
-        keywords: 'education, courses',
-        imageUrl: 'https://storage.googleapis.com/mayash/website/mayash-title.png',
-        type: 'website',
-        url: request.path,
-        site_name: 'Transforming Education',
-        app: '',
-        initialState: '',
-        PRODUCTION: NODE_ENV === 'production',
-      };
-
-      if (!renderProps) {
-        context.app = renderToString(<NotFoundPage />);
-        return reply.view('index', context);
-      }
-
-      let store = configureStore();
-      const initialState = store.getState();
-      context.initialState = JSON.stringify(initialState);
-
-      const { isSignedIn, id, token } = request.state;
-
-      if (isSignedIn === 'true') {
-        const { user } = initialState;
-        store = configureStore({
-          ...initialState,
-          user: {
-            ...user,
-            isSignedIn: true,
-            id: parseInt(id, 10),
-            token,
-          },
-        });
-      }
-      context.initialState = JSON.stringify(store.getState());
-
-      context.app = renderToString(
-        <Provider store={store}>
-          <RouterContext {...renderProps} />
-        </Provider>,
-      );
+    if (!renderProps) {
+      context.app = renderToString(<NotFoundPage />);
       return reply.view('index', context);
-    });
-  },
+    }
+
+    let store = configureStore();
+    const initialState = store.getState();
+    context.initialState = JSON.stringify(initialState);
+
+    const { isSignedIn, id, token } = request.state;
+
+    if (isSignedIn === 'true') {
+      const { user } = initialState;
+      store = configureStore({
+        ...initialState,
+        user: {
+          ...user,
+          isSignedIn: true,
+          id: parseInt(id, 10),
+          token,
+        },
+      });
+    }
+    context.initialState = JSON.stringify(store.getState());
+
+    context.app = renderToString(
+      <Provider store={store}>
+        <RouterContext {...renderProps} />
+      </Provider>,
+    );
+    return reply.view('index', context);
+  }),
 };
