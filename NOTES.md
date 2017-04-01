4a592291d7d46855ecb41afb20eb900c85f39d48
Himank Barve <hbarve1592@gmail.com>
2017-04-01 23:40:49 +0530

    .npmrc file is created, NOTES.md file will keep all the imp. link, code version is changed to 3.0.0-alpha-0

---
+++ NOTES.md
@@ -0,0 +1,5 @@
+https://www.codementor.io/mattgoldspink/nodejs-best-practices-du1086jja
+
+https://docs.travis-ci.com/user/deployment/google-app-engine/
+
+https://kubernetes.io
--- src/server/storage/setup.js
+++
@@ -1,8 +0,0 @@
-
-import { storage } from 'google-cloud';
-import { gcloud } from '../config';
-
-export const BUCKET_NAME = 'mayash';
-const { projectId } = gcloud;
-
-export default storage({ projectId });
