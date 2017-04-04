80b506bab84c8c029e4483a07abd685bc493e0f9
Himank Barve <hbarve1592@gmail.com>
2017-04-05 01:08:07 +0530

    redux folder is removed.

--- redux/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint import/no-extraneous-dependencies: "warn" */
-
-import remotedev from 'remotedev-server';
-
-// This server will help Remote-Redux to work.
-remotedev({ hostname: 'localhost', port: 5002 });
