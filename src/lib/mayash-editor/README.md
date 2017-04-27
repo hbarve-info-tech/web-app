57796ebe6202f630357ca2cd1793a2cb1958219f
Himank Barve <hbarve1592@gmail.com>
2017-04-27 07:38:36 +0530

    MayashEditor is renamed to mayash-editor

---
+++ src/lib/mayash-editor/README.md
@@ -0,0 +1,3 @@
+#Mayash Editor
+
+This is an text editor build on top of draft-js and is customised for Mayash Webapp requirements.
--- src/lib/mayash-editor/index.js
+++ src/lib/mayash-editor/index.js
@@ -2,4 +2,7 @@ import Editor from './Editor';
 
 export { createEditorState } from './EditorState';
 
+export { convertToString } from './functions';
+
+
 export default Editor;
