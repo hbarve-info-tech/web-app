431d147365df07d853b768b35043ec16c288d979
Himank Barve <hbarve1592@gmail.com>
2017-04-29 13:51:30 +0530

    CoursePage Introduction is updated.

--- src/client/components/CoursePage/Introduction.js
+++ src/client/components/CoursePage/Introduction.js
@@ -6,14 +6,36 @@ import PropTypes from 'react/lib/ReactPropTypes';
 class CoursePage extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    const { course } = props;
+    this.state = {
+      course,
+      edit: false,
+    };
   }
 
   render() {
+    const { course, edit } = this.state;
 
     return (
-      <div>
-        introduction.
+      <div className="mdl-grid">
+        <div className="mdl-cell mdl-cell--8-col">
+          <div className="mdl-card mdl-shadow--4dp" style={{width: '100%', minHeight: '50px'}}>
+            <div className="mdl-card__title">
+              <div className="mdl-card__title-text">{course.title}</div>
+            </div>
+            <div className="mdl-card__supporting-text">
+              {course.description}
+            </div>
+            <div className="mdl-card__menu">
+              <button
+                className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect"
+                onClick={() => this.setState({ edit: !edit })}
+              >
+                <i className="material-icons">{edit ? 'save' : 'edit'}</i>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
