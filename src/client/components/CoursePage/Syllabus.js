7f5b946b9331d04ca7c50551d1fb77b2ecab5b2d
Himank Barve <hbarve1592@gmail.com>
2017-04-29 13:26:39 +0530

    CoursePage component is updated.

---
+++ src/client/components/CoursePage/Syllabus.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import Component from 'react/lib/ReactComponent';
+import PropTypes from 'react/lib/ReactPropTypes';
+
+class CoursePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+
+    return (
+      <div>
+        Syllabus.
+      </div>
+    );
+  }
+}
+
+
+export default CoursePage;
--- src/client/components/CoursePage/index.js
+++ src/client/components/CoursePage/index.js
@@ -7,8 +7,15 @@ import { connect } from 'react-redux';
 
 import actions from '../../actions';
 
+import HeaderRow from '../Header/HeaderRow';
 import ErrorPage from '../ErrorPage';
 
+import Introduction from './Introduction';
+import Syllabus from './Syllabus';
+import Modules from './Modules';
+import Discussions from './Discussions';
+import TestYourself from './TestYourself';
+import Feedback from './Feedback';
 
 class CoursePage extends Component {
   constructor(props) {
@@ -18,38 +25,16 @@ class CoursePage extends Component {
 
   render() {
     const { courseId } = this.props.routeParams;
-    const user = this.props.elements[0];
     const course = this.props.courses.find(a => a.courseId === parseInt(courseId, 10));
 
-    if (course.statusCode !== 200) {
+    if (!course || course.statusCode !== 200) {
       return (<ErrorPage {...course} />);
     }
 
     return (
       <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header mdl-layout--no-drawer-button">
         <header className="mdl-layout__header">
-          <div className="mdl-layout__header-row">
-            <span className="mdl-layout-title">Mayash</span>
-
-            <div className="mdl-layout-spacer" />
-
-            <button
-              id="sign-in-dropdown"
-              className="mdl-button mdl-js-button mdl-button--icon"
-            >
-              <i className="material-icons">more_vert</i>
-            </button>
-            <ul
-              className="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect"
-              htmlFor="sign-in-dropdown"
-            >
-              <li
-                className="mdl-menu__item"
-              >
-                Sign Out
-              </li>
-            </ul>
-          </div>
+          <HeaderRow/>
 
           <div className="mdl-layout__tab-bar mdl-js-ripple-effect">
             <a href="#scroll-tab-1" className="mdl-layout__tab is-active">Introduction</a>
@@ -62,22 +47,34 @@ class CoursePage extends Component {
         </header>
         <main className="mdl-layout__content">
           <section className="mdl-layout__tab-panel is-active" id="scroll-tab-1">
-            <div className="page-content">Tab 1</div>
+            <div className="page-content">
+              <Introduction course={course} {...this.props} />
+            </div>
           </section>
           <section className="mdl-layout__tab-panel" id="scroll-tab-2">
-            <div className="page-content">Tab 2</div>
+            <div className="page-content">
+              <Syllabus course={course} {...this.props} />
+            </div>
           </section>
           <section className="mdl-layout__tab-panel" id="scroll-tab-3">
-            <div className="page-content">Tab 3</div>
+            <div className="page-content">
+              <Modules course={course} {...this.props} />
+            </div>
           </section>
           <section className="mdl-layout__tab-panel" id="scroll-tab-4">
-            <div className="page-content">Tab 3</div>
+            <div className="page-content">
+              <Discussions {...this.props} />
+            </div>
           </section>
           <section className="mdl-layout__tab-panel" id="scroll-tab-5">
-            <div className="page-content">Tab 3</div>
+            <div className="page-content">
+              <TestYourself course={course} {...this.props} />
+            </div>
           </section>
           <section className="mdl-layout__tab-panel" id="scroll-tab-6">
-            <div className="page-content">Tab 3</div>
+            <div className="page-content">
+              <Feedback course={course} {...this.props} />
+            </div>
           </section>
         </main>
       </div>
@@ -91,11 +88,6 @@ CoursePage.propTypes = {
   }).isRequired,
   elements: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
-  // updateCourse: PropTypes.func.isRequired,
-  getCourse: PropTypes.func.isRequired,
-  // fetchModules: PropTypes.func.isRequired,
-  // updateModule: PropTypes.func.isRequired,
-  // createModule: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => state;
