abe3a129a0e9d888f214fc2926d5d420d36d6ed2
Himank Barve <hbarve1592@gmail.com>
2017-05-07 20:12:23 +0530

    signIn and signOut bug is fixed.

--- src/client/components/Header/HeaderRow.js
+++ src/client/components/Header/HeaderRow.js
@@ -1,38 +1,49 @@
 
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import actions from '../../actions';
 
 import style from './style';
 
-const HeaderRow = ({}) => (
-  <div className="mdl-layout__header-row">
+const signInFunc = () => window.location.href = '/signin';
+
+const HeaderRow = ({ elements, signOut }) => {
+  const user = elements[0];
+
+  return (
+    <div className="mdl-layout__header-row">
     <span className="mdl-layout-title">
       <a href="/" style={style.headerTitle}>
         Mayash
       </a>
     </span>
 
-    <div className="mdl-layout-spacer" />
+      <div className="mdl-layout-spacer" />
 
-    <button
-      id="sign-in-dropdown"
-      className="mdl-button mdl-js-button mdl-button--icon"
-    >
-      <i className="material-icons">more_vert</i>
-    </button>
-    <ul
-      className="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect"
-      htmlFor="sign-in-dropdown"
-    >
-      <li
-        className="mdl-menu__item"
-        onClick={() => {
-          window.location.href = '/signin';
-        }}
+      <button
+        id="sign-in-dropdown"
+        className="mdl-button mdl-js-button mdl-button--icon"
       >
-        Sign Out
-      </li>
-    </ul>
-  </div>
-);
+        <i className="material-icons">more_vert</i>
+      </button>
+      <ul
+        className="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect"
+        htmlFor="sign-in-dropdown"
+      >
+        <li
+          className="mdl-menu__item"
+          onClick={user.isSignedIn === true ? signOut : signInFunc}
+        >
+          {user.isSignedIn === true ? 'Sign Out' : 'Sign In'}
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default HeaderRow;
+const mapStateToProps = state => state;
+const mapDispatchToProps = (dispatch) => bindActionCreators(actions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(HeaderRow);
