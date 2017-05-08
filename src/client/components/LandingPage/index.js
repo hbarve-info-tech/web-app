f6e00a73e6f8f2848b693a3d0075da4dd72137e8
Himank Barve <hbarve1592@gmail.com>
2017-05-08 15:46:50 +0530

    Landing page is updated.

--- src/client/components/LandingPage/index.js
+++ src/client/components/LandingPage/index.js
@@ -24,27 +24,15 @@ const Introduction = () => (
           </h2>
         </div>
         <div className="mdl-card__supporting-text" style={style.card.description}>
-          <p>
-            <b>Mayash</b> is a team of enthusiastic younger of Bharath committed to solve the challenges of our
-            society. As there are so many challenges are out there, problems in education system and media & Governance
-            are the burning issue right now.
-          </p>
-          <p>
-            We are attempting to solve two major problems in our society:
-            1. Problems in Education system.
-            2. Governance & Media.
-          </p>
-
-          <h4>First let Talk about <b>Education System:</b></h4>
           <article>
             <p>
-              Education is considered the most important part of any human being and for us definition of Education is to
-              bring the best of every human being. But our present education system does not focus on this, for now
-              education system only focus on marks not in skill development.
+              Mayash is the leading service provider of various educational courses for undergraduates and post graduates.
+              We are looking for the improvement in educational system, where we will be providing them with the updated
+              knowledge and the latest technologies for students to upgrade their skills.
             </p>
             <p>
-              <b>Mayash</b> is formed to improve this education system and make it much more efficient so that every
-              student can get the best of what they want to learn and grow.
+              The founder of this website is Mr. Himank Honey Barve. The website is concerned with the improvement of education system within the country
+              and bringing awareness among the society.
             </p>
           </article>
         </div>
@@ -53,25 +41,16 @@ const Introduction = () => (
   </div>
 );
 
-const Motivation = () => (
+const Vision = () => (
   <div className="mdl-grid">
     <div className="mdl-cell mdl-cell--12-col">
-      <div className="mdl-card mdl-shadow--4dp" style={style.card}>
-        <div className="mdl-card__media" style={style.card.media}>
-          <img
-            src="https://storage.googleapis.com/mayash/website/Mayash%20Motivation%20(1).jpg"
-            alt=""
-            style={style.card.media.image}
-          />
+      <div className="mdl-card mdl-shadow--4dp" >
+        <div className="mdl-card__title">
+          <h2 className="mdl-card__title-text">Vision</h2>
         </div>
-        <div className="mdl-card__title" style={style.card.title}>
-          <h2 className="mdl-card__title-text">Motivation</h2>
-        </div>
-        <div className="mdl-card__supporting-text" style={style.card.description}>
+        <div className="mdl-card__supporting-text">
           <p>
-            <b>Bharath</b> as 'The land of knowledge' has known for centuries but in last 800 years we
-            have gone through lot of suffering and changes which has pulled us backward. We
-            are committed to return that glories.
+            Providing quality education to each and everyone.
           </p>
         </div>
       </div>
@@ -156,78 +135,148 @@ const Team = () => {
   );
 };
 
-const ProductAndServices = () => {
-  const members = [
-    {
-      id: 1,
-      title: 'World Class Education at your door step.',
-      description: '',
-    },
-    {
-      id: 2,
-      title: 'Language Independence.',
-      description: 'We are working on to make education free from language restriction.',
-    },
-    {
-      id: 3,
-      title: 'Practical knowledge.',
-      description: 'Focus on Practical understanding rather then just theoretical knowledge.',
-    },
-    {
-      id: 4,
-      title: 'Connect with other educational institutes.',
-      description: 'We are making this platform to connect all educational institute.',
-    },
-    {
-      id: 5,
-      title: 'All fields of education are covered.',
-      description: 'We are covering all fields of education, not just academic but music, dance, sports etc.',
-    },
-  ];
-
+const Services = () => {
   return (
     <div className="mdl-grid">
-      {members.map(member => (
-        <div className="mdl-cell mdl-cell--3-col" key={member.id}>
-          <div className="mdl-card mdl-shadow--4dp" style={{width: '100%', minHeight: '50px'}}>
-            <div className="mdl-card__title">
-              <h2 className="mdl-card__title-text">{member.title}</h2>
-            </div>
-            <div className="mdl-card__supporting-text">
-              {member.description}
-            </div>
+      <div className="mdl-cell mdl-cell--12-col">
+        <div className="mdl-card mdl-shadow--4dp">
+          <div className="mdl-card__title">
+            <h2 className="mdl-card__title-text">Services for Students</h2>
+          </div>
+          <div className="mdl-card__supporting-text">
+            <h5>Services:</h5>
+            <ul>
+              <li>Subject Notes.</li>
+              <li>Test Series.</li>
+              <li>Discussion Channel.</li>
+              <li>Career Guidance.</li>
+            </ul>
+            <h5>Pricing:</h5>
+            <ul>
+              <li>Registration: Free</li>
+              <li>Basic Services: Free</li>
+              <li>Specific Courses: Charged</li>
+              <li>Test Series: Charged</li>
+            </ul>
           </div>
         </div>
-      ))}
+      </div>
+
+      <div className="mdl-cell mdl-cell--12-col">
+        <div className="mdl-card mdl-shadow--4dp">
+          <div className="mdl-card__title">
+            <h2 className="mdl-card__title-text">Services for Teachers & Professors</h2>
+          </div>
+          <div className="mdl-card__supporting-text">
+            <h5>Services:</h5>
+            <ul>
+              <li>Create and Share knowledge is the form of courses.</li>
+              <li>Direct interaction with students in single platform.</li>
+            </ul>
+            <h5>Pricing:</h5>
+            <ul>
+              <li>Registration: Free</li>
+              <li>Basic Services: Free</li>
+              <li>Premium Services: Charged</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="mdl-cell mdl-cell--12-col">
+        <div className="mdl-card mdl-shadow--4dp">
+          <div className="mdl-card__title">
+            <h2 className="mdl-card__title-text">Services for Educational Institutes</h2>
+          </div>
+          <div className="mdl-card__supporting-text">
+            <h5>Services:</h5>
+            <ul>
+              <li>Institute will get a private virtual classroom network.</li>
+              <li>Teacher can share their course, give assignment, take class test etc.</li>
+              <li>Students can interact with their teachers in single platform.</li>
+              <li>Students and teachers will get the updated technology to study, and improve the education.</li>
+              <li>Campus placement study material and updates.</li>
+              <li>Alumni connect.</li>
+            </ul>
+            <h5>Pricing:</h5>
+            <ul>
+              <li>Registration: Free</li>
+              <li>Teacher's Registration: Free</li>
+              <li>Student's Registration: 2000/- per head</li>
+              <li>Basic Services: Free</li>
+              <li>Premium Services: Charged</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="mdl-cell mdl-cell--12-col">
+        <div className="mdl-card mdl-shadow--4dp">
+          <div className="mdl-card__title">
+            <h2 className="mdl-card__title-text">Services for Organisations & Companies</h2>
+          </div>
+          <div className="mdl-card__supporting-text">
+            <h5>Services:</h5>
+            <ul>
+              <li>Can post their job vacancy with details.</li>
+              <li>Can guide people for preparation of jobs in your company.</li>
+              <li>Sharing the specific knowledge and technology.</li>
+              <li>If you are working on a new technology, you can create course for it and share with people.</li>
+            </ul>
+            <h5>Pricing:</h5>
+            <ul>
+              <li>Registration: Free</li>
+              <li>Basic Services: Free</li>
+              <li>Premium Services: Charged</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="mdl-cell mdl-cell--12-col">
+        <div className="mdl-card mdl-shadow--4dp">
+          <div className="mdl-card__title">
+            <h2 className="mdl-card__title-text">General Public</h2>
+          </div>
+          <div className="mdl-card__supporting-text">
+            <h5>Services:</h5>
+            <ul>
+              <li>Posting and sharing their thoughts.</li>
+              <li>Get updated knowledge in any field.</li>
+            </ul>
+            <h5>Pricing:</h5>
+            <ul>
+              <li>Registration: Free</li>
+              <li>Basic Services: Free</li>
+              <li>Premium Services: Charged</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-const MayashMedia = () => (
+const Hiring = () => (
   <div className="mdl-grid">
     <div className="mdl-cell mdl-cell--12-col">
-      <div className="mdl-card mdl-shadow--4dp" style={{width: '100%', minHeight: '50px'}}>
+      <div className="mdl-card mdl-shadow--4dp">
         <div className="mdl-card__title">
-          <h2 className="mdl-card__title-text">Mayash Media</h2>
+          <h2 className="mdl-card__title-text">
+            Join our team to make a positive difference in society.
+          </h2>
         </div>
         <div className="mdl-card__supporting-text">
-          <article>
-            <p>
-              We are working on a second project also, which is to improve media and governance in our country.
-            </p>
-            <p>
-              We are regularly getting fake news or some biased prospective of any incident.
-            </p>
-            <p>
-              Here are Mayash Media, we are trying to create news channel for every single locality. Here people can see
-              news from any locality and can report news also.
-            </p>
-            <p>
-              We are looking for enthusiastic people who are wants to make world a better place to live. If you are
-              interested in joining us. Contact us <a href="https://www.facebook.com/mayashmedia" target="_blank">
-              Mayash Media</a>
-            </p>
-          </article>
+          <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSfOhyNRRWtPzb0TEDYpcZ2HbdAhdF-54Wu49_gB9Io1ISM5kw/viewform?embedded=true"
+            width="100%"
+            height="750px"
+            frameBorder={0}
+            marginHeight={0}
+            marginWidth={0}
+          >
+            Loading...
+          </iframe>
         </div>
       </div>
     </div>
@@ -237,7 +286,7 @@ const MayashMedia = () => (
 const ContactUs = () => (
   <div className="mdl-grid">
     <div className="mdl-cell mdl-cell--12-col">
-      <div className="mdl-card mdl-shadow--4dp" style={{width: '100%', minHeight: '50px'}}>
+      <div className="mdl-card mdl-shadow--4dp">
         <div className="mdl-card__media">
           <img
             src="https://storage.googleapis.com/mayash/website/mayash-contact-us.jpg"
@@ -249,6 +298,13 @@ const ContactUs = () => (
           <h2 className="mdl-card__title-text">
             Contact Us
           </h2>
+        </div>
+        <div className="mdl-card__supporting-text">
+          <p><b>Name: </b> Himank Honey Barve</p>
+          <p><b>Email: </b> hbarve1592@gmail.com</p>
+          <p><b>Mobile No.: </b> 9097535837</p>
+          <p><b>Facebook: </b> https://facebook.com/hbarve1</p>
+          <p><b>Twitter: </b> https://twitter.com/hbarve1</p>
         </div>
         <div className="mdl-card__supporting-text">
           <iframe
@@ -267,53 +323,57 @@ const ContactUs = () => (
   </div>
 );
 
-const LandingPage = () => {
-  return (
-    <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header mdl-layout--no-drawer-button">
-      <header className="mdl-layout__header">
-        <HeaderRow />
+const LandingPage = () => (
+  <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header mdl-layout--no-drawer-button">
+    <header className="mdl-layout__header">
+      <HeaderRow />
 
-        <div className="mdl-layout__tab-bar mdl-js-ripple-effect">
-          <a href="#introduction" className="mdl-layout__tab is-active">Introduction</a>
-          <a href="#motivation" className="mdl-layout__tab">Motivation</a>
-          <a href="#team" className="mdl-layout__tab">Team</a>
-          <a href="#product-and-services" className="mdl-layout__tab">Product & Services</a>
-          <a href="#contact-us" className="mdl-layout__tab">Contact Us</a>
+      <div className="mdl-layout__tab-bar mdl-js-ripple-effect">
+        <a href="#introduction" className="mdl-layout__tab is-active">Introduction</a>
+        <a href="#vision" className="mdl-layout__tab">Vision</a>
+        <a href="#team" className="mdl-layout__tab">Team</a>
+        <a href="#services" className="mdl-layout__tab">Services</a>
+        <a href="#hiring" className="mdl-layout__tab">We are Hiring</a>
+        <a href="#contact-us" className="mdl-layout__tab">Contact Us</a>
+      </div>
+    </header>
+    <main className="mdl-layout__content">
+      <p style={{textAlign: 'center', position: 'fixed', bottom: '0', zIndex: 999 }}>
+        <b>Note:</b> We are in Beta stage right now, our website is in active development. If you are having any trouble feel free
+        to contact us.
+      </p>
+      <section className="mdl-layout__tab-panel is-active" id="introduction">
+        <div className="page-content">
+          <Introduction/>
         </div>
-      </header>
-      <main className="mdl-layout__content">
-        <p style={{textAlign: 'center', position: 'fixed', bottom: '0', zIndex: 999, }}>
-          <b>Note:</b> We are in Beta stage right now, our website is in active development. If you are having any trouble feel free
-          to contact us.
-        </p>
-        <section className="mdl-layout__tab-panel is-active" id="introduction">
-          <div className="page-content">
-            <Introduction/>
-          </div>
-        </section>
-        <section className="mdl-layout__tab-panel" id="motivation">
-          <div className="page-content">
-            <Motivation/>
-          </div>
-        </section>
-        <section className="mdl-layout__tab-panel" id="team">
-          <div className="page-content">
-            <Team/>
-          </div>
-        </section>
-        <section className="mdl-layout__tab-panel" id="product-and-services">
-          <div className="page-content">
-            <ProductAndServices/>
-          </div>
-        </section>
-        <section className="mdl-layout__tab-panel" id="contact-us">
-          <div className="page-content">
-            <ContactUs/>
-          </div>
-        </section>
-      </main>
-    </div>
-  );
-};
+      </section>
+      <section className="mdl-layout__tab-panel" id="vision">
+        <div className="page-content">
+          <Vision/>
+        </div>
+      </section>
+      <section className="mdl-layout__tab-panel" id="team">
+        <div className="page-content">
+          <Team/>
+        </div>
+      </section>
+      <section className="mdl-layout__tab-panel" id="services">
+        <div className="page-content">
+          <Services/>
+        </div>
+      </section>
+      <section className="mdl-layout__tab-panel" id="hiring">
+        <div className="page-content">
+          <Hiring/>
+        </div>
+      </section>
+      <section className="mdl-layout__tab-panel" id="contact-us">
+        <div className="page-content">
+          <ContactUs/>
+        </div>
+      </section>
+    </main>
+  </div>
+);
 
 export default LandingPage;
